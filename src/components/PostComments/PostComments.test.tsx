8f0db6c83dd3559030b16c3e32f2edd1de55b4c4
ablacs <arthurblcs@yahoo.com.br>
2025-06-26 18:25:24 -0300

    Adicionando testes para verificar comentários

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,26 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
-import PostComment from '.';
+import { fireEvent, render, screen } from "@testing-library/react";
+import PostComment from ".";
 
-describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
+describe("Teste para o componente PostComment", () => {
+  test("Deve renderizar o componente corretamente", () => {
+    render(<PostComment />);
+    expect(screen.getByText("Comentar")).toBeInTheDocument();
+  });
+
+  test("Deve adicionar um comentário", () => {
+    render(<PostComment />);
+    const comentario1 = "comentario 1";
+    const comentario2 = "comentario 2";
+    fireEvent.change(screen.getByTestId("comment-input"), {
+      target: { value: comentario1 },
     });
+    fireEvent.click(screen.getByText("Comentar"));
+    fireEvent.change(screen.getByTestId("comment-input"), {
+      target: { value: comentario2 },
+    });
+    fireEvent.click(screen.getByText("Comentar"));
+    expect(screen.getByText(comentario1)).toBeInTheDocument();
+    expect(screen.getByText(comentario2)).toBeInTheDocument();
+    expect(screen.getAllByTestId("comment-item")).toHaveLength(2);
+  });
 });
